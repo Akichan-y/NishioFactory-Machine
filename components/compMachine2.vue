c1224894bceb4e31a96234444947102366c2db76
Akichan-y <a.yamazaki1971@gmail.com>
2021-03-25 16:09:45 +0900

    機械毎マシンアワーグラフ & ハンバーガーメニュー
    視覚的にわかりやすくするために、グラフの棒を色分けしてみた。

--- components/compMachine2.vue
+++ components/compMachine2.vue
@@ -225,12 +225,23 @@ export default {
       Line_chartData: {
         labels: [
         ],
+        
         // データ詳細
         datasets: [
           {
             label: ["RUN"],
             // backgroundColor: ['aqua','red','blue','lime','fuchsia','green','yellow','maroon','navy'],
-            backgroundColor: "limegreen",
+            // backgroundColor: "limegreen",
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.9)',
+                'rgba(54, 162, 235, 0.9)',
+                'rgba(255, 159, 64, 0.9)',
+                'rgba(75, 192, 192, 0.9)',
+                'rgba(153, 102, 255, 0.9)',
+                'rgba(255, 206, 86, 0.9)',
+                'rgba(200, 100, 86, 0.9)',
+                
+            ],
             data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
           },
           // {
